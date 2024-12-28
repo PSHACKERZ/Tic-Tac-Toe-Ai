90007076eb1c94fcc96f57998d06f4055eadd318
Prince Sahni <sahnisunita36@gmail.com>
2024-12-29 02:50:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,5 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <App/>
-=======
-    <App />
->>>>>>> d5f3fa0a1237bb1096d0274ab439b782326236ef
   </React.StrictMode>
 );
