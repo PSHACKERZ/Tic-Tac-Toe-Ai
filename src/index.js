fe6a905d3cb581c1343216861903ace5f05b0d8f
Prince Sahni <sahnisunita36@gmail.com>
2024-12-29 03:22:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import TicTacToe from './TicTacToe';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <TicTacToe />
   </React.StrictMode>
 );
